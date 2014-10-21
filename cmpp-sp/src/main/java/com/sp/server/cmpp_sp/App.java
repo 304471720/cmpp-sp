273e3fbacf917ecdc9c5c748d39c6e10fb0ad6c8
n550 <n550@asus>
2014-10-22 00:11:44 +0800

    xxx

---
+++ cmpp-sp/src/main/java/com/sp/server/cmpp_sp/App.java
@@ -0,0 +1,13 @@
+package com.sp.server.cmpp_sp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
